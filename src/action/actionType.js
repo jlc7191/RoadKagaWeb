488e563623c1442c5e983a702544497761867bdc
jlc7191 <44053726+jlc7191@users.noreply.github.com>
2019-06-27 11:04:55 +0800

    0627-3

--- src/action/actionType.js
+++
@@ -1,7 +0,0 @@
-// 把標籤改成常數，有問題才會跳錯，不至於找不到bug
-export const POST_MSG = 'POST_MSG'
-export const PUT_MSG = 'PUT_MSG'
-export const DEL_MSG = 'DEL_MSG'
-export const POST_CMSG = 'POST_CMSG'
-export const PUT_CMSG = 'PUT_CMSG'
-export const DEL_CMSG = 'DEL_CMSG'
